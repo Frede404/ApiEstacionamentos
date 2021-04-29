5ce84c163f10b9015e4809d392f5d2a1abacadd3
LeandroEmanuel <44574587+LeandroEmanuel@users.noreply.github.com>
2021-04-30 00:26:55 +0100

    modelo colecao matriculas

--- Projeto/models/estacionamentosModel.js
+++ Projeto/models/estacionamentosModel.js
@@ -1,21 +1,17 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-let RegistoSchema = new Schema({
+let MatriculasSchema = new Schema({
     _id: {
         type: Number, 
         generated: false, 
         default: 1
     },
-    nome: {
-        type: String,
-        required: true
-    },
-    email: {
+    nMatricula: {
         type: String,
         required: true
     }
 });
 
 //Exportar o modelo
-module.exports = mongoose.model('Registo', RegistoSchema);
+module.exports = mongoose.model('Matriculas', MatriculasSchema);
