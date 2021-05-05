71cc04903b6b73a02ca693b232f074d0cc1152cf
Frede404 <61785485+Frede404@users.noreply.github.com>
2021-05-05 22:15:43 +0100

    populate

--- Projeto/controllers/AdministracaoController.js
+++ Projeto/controllers/AdministracaoController.js
@@ -7,6 +7,8 @@ exports.AddMatriculas = async function(req, res){
 	
     let quantidade = req.params.n
 
+    let listaMatriculas=[]
+
     for(i=1; i<=quantidade; i++){
         //gera a matricula caracter a caracter aleatoriamente
         let numeroa = Math.floor(Math.random() * (9)) ;
@@ -26,15 +28,11 @@ exports.AddMatriculas = async function(req, res){
              String.fromCharCode(letrab) + '-' + numeroc + numerod,
         });
         
-        //insere a matricula
-        registo.save(function(err){
-            
-            if(err){
-                throw err;
-            }
-        })
+        listaMatriculas.push(registo)
     }
 
+    MatriculaModel.insertMany(listaMatriculas)
+    
     res.send('Populate completo, pode ter de aguardar alguns minutos para estar acecivel na BD sao ' + (i-1) + ' dados'
     + '\n' + 'Nao desligue o servidor ou a importacao dos dados será cancelada!')
 }
@@ -56,9 +54,9 @@ exports.AddEstacionamentos = async function(req, res){
         //lotacao do parque
         LugaresModel.findById(1,function(req, registo){
             let lugares = parseInt('' + registo.maximo, 10);
-            let PEntradas = 50;
+            let PEntradas = 60;
             let LMinimo = -5;
-            
+
             let data = new Date();
             let dia = data.getDate();
             let mes = data.getMonth()+1;
@@ -75,10 +73,28 @@ exports.AddEstacionamentos = async function(req, res){
             let anoI = inicio.getFullYear();
 
             let introduz = diaI + '/' + mesI + '/' + anoI;
-            let introduzInv = anoI + '/' + mesI + '/' + diaI;
 
-            let vagas = lugares;
+            let diaIns=''
+            let mesIns=''
+
+            if(mesI<10){
+                mesIns='/0'+mesI
+            }else{
+                mesIns='/'+mesI
+            }
+            if(diaI<10){
+                diaIns='/0'+diaI
+            }else{
+                diaIns='/'+diaI
+            }
+
+            let introduzInv = anoI + mesIns + diaIns;
+            //let introduzInv = anoI + '/' + mesI + '/' + diaI;
+
+            let vagas = lugares/2;
             let correnteID=1
+            let auxvagas=mesI
+            let NovasEntradas=[]
 
             //ciclo para percorrer desde a data inicial até a data atual
             while(hoje != introduz){
@@ -86,15 +102,15 @@ exports.AddEstacionamentos = async function(req, res){
                 let entradas = Math.floor(Math.random()*(lugares+20)+1); 
                 //inicializacao da quantidade de carros que entram no dia
                 let entrada = 1
-
+                if(auxvagas!=mesI){
+                vagas = lugares/2;}
                 //ciclo para registar as entradas do dia a introduzir
                 for(i=1; i<=entradas; i++){
                     //variavel aleatoria para saber se vai registar uma entrada
-                    let tipo=Math.floor(Math.random()*100+1)
-
+                    let tipo=Math.floor(Math.random()*100+1);
                     //forca nao haver saidas ou entradas de mais
-                    if(vagas < LMinimo && tipo <= PEntradas){
-                        tipo=100
+                    if(vagas <= LMinimo && tipo <= PEntradas){
+                        tipo=100                        
                     }else if(vagas == lugares && tipo > PEntradas){
                         tipo=1
                     }
@@ -124,18 +140,12 @@ exports.AddEstacionamentos = async function(req, res){
                             lotacao: vagas,
                             nEntrada: entrada
                         })
-
-                        NovaEntrada.save(function (err){
-                            if(err){
-                                throw err;
-                            }
-                        })
+                        NovasEntradas.push(NovaEntrada)
 
                         //atualizacao de variaveis
                         vagas=vagas-1
                         entrada = entrada+1
                         correnteID=correnteID+1
-
                     }else{
                         vagas=vagas+1
                     }
@@ -148,16 +158,35 @@ exports.AddEstacionamentos = async function(req, res){
                 anoI = inicio.getFullYear();
 
                 introduz = diaI + '/' + mesI + '/' + anoI;
-                introduzInv = anoI + '/' + mesI + '/' + diaI;
+
+                diaIns=''
+                mesIns=''
+
+                if(mesI<10){
+                    mesIns='/0'+mesI
+                }else{
+                    mesIns='/'+mesI
+                }
+                if(diaI<10){
+                    diaIns='/0'+diaI
+                }else{
+                    diaIns='/'+diaI
+                }
+
+                introduzInv = anoI + mesIns + diaIns;
+                //introduzInv = anoI + '/' + mesI + '/' + diaI;
             }
-            
+
+            console.log(NovasEntradas)
+            RegistoEntradasModel.insertMany(NovasEntradas)
+
             res.send('Populate completo, pode ter de aguardar alguns minutos para estar acecivel na BD sao ' + (correnteID-1) + ' dados'
             + '\n' + 'Nao desligue o servidor ou a importacao dos dados será cancelada!')
         })
     })
-}    
+}   
 
-exports.DelEstacionamentos = function(req, res){
+exports.DelEstacionamentos = function(req, res){ 
     RegistoEntradasModel.remove(function(err){
         if(err){
             throw err;
